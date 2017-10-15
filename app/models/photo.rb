8c2a4e2fee3ef8c6fc1326f8855527d57e3bacfa
waqar <whassan25@gmail.com>
2017-10-15 10:39:38 +0500

    spec/photos_spec.rb -e rq03

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,4 +4,13 @@ class Photo
 		Mongoid::Clients.default
 	end
 	attr_accessor :id , :location , :contents
+	def initialize params = {}
+		if !params.nil?
+			@id = params[:_id].nil? ? params[:id] : params[:_id].to_s
+			@location = Point.new(params[:metadata][:location]) if !params[:metadata].nil?
+		else
+			Photo.new
+		end
+
+	end
 end
